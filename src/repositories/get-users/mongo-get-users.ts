0533622def484dcaf9f8c469ef9688c88be60b15
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-11 21:49:49 -0300

    Feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,19 @@
+import { MongoClient } from "../../database/mongo";
 import { iGetUserRepository } from "../../interfaces/users.types";
 import { User } from "../../models/users.models";
 
 export class MongoGetUsersRepository implements iGetUserRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Gabriel",
-        lastName: "Ferreira",
-        email: "gabrielrf@gmail.com",
-        password: "123456",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    const user_result = users.map(({ _id, ...users }) => ({
+      ...users,
+      id: _id.toHexString(),
+    }));
+
+    return user_result;
   }
 }
